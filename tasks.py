212c2bf4ca7151ed7e6be1b01be106632355d2ac
mrgrit <vulcanus@naver.com>
2017-07-04 22:20:06 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,6 +1,18 @@
 from microsoftbotframework import ReplyToActivity
+import json
+import requests
+from datetime import datetime
 
 def echo_response(message):
   if message["type"] == "message":
     ReplyToActivity(fill=message,
                     text=message["text"]).send()
+    
+def bc_response(message):
+  if message["type"] == "bitcoin" :
+    now = datetime.now()
+    res = requests.get("https://api.korbit-test.com/v1/ticker?currency_pair=btc_krw")
+    rej = res.json()
+    rep = datetime.now() + 'now' + rej['last'] + 'BTC/KRW.'
+    ReplyToActivity(fill=rep,
+                    text=rep["text"]).send()
