da94f8833a36b9dc4e7b51889fac39c73d1634ab
mrgrit <vulcanus@naver.com>
2017-07-09 18:50:35 +0900

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -8,11 +8,12 @@ def echo_response(message):
     ReplyToActivity(fill=message,
                     text=message["text"]).send()
     
-def bc_response(message):
+def bc_response(message):  
   if message["type"] == "message" :
-    now = datetime.now()
-    res = requests.get("https://api.korbit-test.com/v1/ticker?currency_pair=btc_krw")
-    rej = res.json()
-    rep = datetime.now() + 'now' + rej['last'] + 'BTC/KRW.'
-    ReplyToActivity(fill=message,
-                    text=message["text"]).send()
+    if "bitcoin" in message["text"]:
+        now = datetime.now()
+        res = requests.get("https://api.korbit-test.com/v1/ticker?currency_pair=btc_krw")
+        rej = res.json()
+        rep = datetime.now() + 'now' + rej['last'] + 'BTC/KRW.'
+        ReplyToActivity(fill=message,
+                        text=rep).send()
